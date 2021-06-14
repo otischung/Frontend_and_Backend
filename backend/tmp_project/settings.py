9444f975697cdce8fd12752fd90e686ddbeb10c7
Oracle Public Cloud User <opc@oracle-vm-20210610.subnet06031622.vcn06031622.oraclevcn.com>
2021-06-14 07:47:25 +0000

    publish by nginx

--- backend/tmp_project/settings.py
+++ backend/tmp_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'otischung.ray-fish.me'
@@ -123,9 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_django/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+STATIC_ROOT = '/var/www/static_django/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
